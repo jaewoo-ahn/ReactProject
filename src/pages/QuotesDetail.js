0f391180f93d4b44e52acdd5c7ba0c5213b74f41
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-02-22 16:51:52 +0900

    add : comments nest route

--- src/pages/QuotesDetail.js
+++ src/pages/QuotesDetail.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {Fragment} from "react";
+import {useParams, Route} from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
 
 function QuotesDetail() {
-  return <h1>QuotesDetail</h1>;
+  const params = useParams();
+  return (
+    <Fragment>
+      <h1>QuotesDetail</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 }
 
 export default QuotesDetail;
