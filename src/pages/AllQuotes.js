e14b14186241610e4eea9494907cf5c6c03689ad
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-02-22 17:29:58 +0900

    add : NewQuote

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,13 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {id: 1, author: "Max", text: "learning react is fun"},
+  {id: 2, author: "Jax", text: "learning react is great"},
+];
 
 function AllQuotes() {
-  return <h1>AllQuotes</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 }
 
 export default AllQuotes;
