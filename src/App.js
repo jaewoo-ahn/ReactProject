9a61cce9f749a04fb1ae93f4b73f4402e75fda40
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-02-23 18:49:22 +0900

    add: suspense

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
+import React, {Suspense} from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
+
 import QuotesDetail from "./pages/QuotesDetail";
 import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
 
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuotesDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuotesDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
